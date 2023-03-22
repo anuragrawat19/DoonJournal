ab28eb05528cdda4a53777c1a48816b1337db71f
anurag.rawat <anurag.rawat@thesocialbytes.com>
2023-03-22 18:39:51 +0530

    adding the mysql db config and other relted changes

--- ContentManagement/views.py
+++ ContentManagement/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def home(request):
     template = 'index.html'
     return render(request,template)
+
+def about_us(request):
+    template = 'about.html'
+    return render(request,template)
